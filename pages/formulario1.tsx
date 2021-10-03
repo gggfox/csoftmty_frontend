37127b9d648999be9e08ec849335eddb2bae8328
Antoniogm0898 <31547248+Antoniogm0898@users.noreply.github.com>
2021-10-03 13:55:16 -0500

    Comenzar la implemetacion de los forms

--- pages/formulario1.tsx
+++ pages/formulario1.tsx
@@ -1,12 +1,11 @@
 import { Button, Flex, Heading, Input, Link, Text, Stack, SimpleGrid} from '@chakra-ui/react'
-import { Box, HStack,  Select, Progress, Spacer, Checkbox} from '@chakra-ui/react'
-import { Table, Th, Tr, Td, Thead, Tbody} from '@chakra-ui/react'
+import { IconButton, Select } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import React from 'react'
 import { NavBar } from '../components/NavBar'
 import styles from '../styles/Home.module.css'
 
-const Signin: NextPage = () => {
+const formulario_01: NextPage = () => {
   return (
     <Flex bg="white_dark" h='100vh' flexDirection="column">
       <NavBar></NavBar>
@@ -52,71 +51,14 @@ const Signin: NextPage = () => {
 
 
           <Button colorScheme="orange" backgroundColor="orange" mb={6}>Proceder</Button>
-
-
-          <Box paddingBlockEnd = "10px">
-            <HStack spacing="24px">
-              <Button colorScheme="gray_dark" backgroundColor="gray_dark" flex="1">Seccion Previa</Button>
-              <Progress hasStripe value={10} colorScheme="orange_trans" backgroundColor="gray_dark" height="46px"  flex="3"/>
-            </HStack>
-          </Box>
-          <Table variant="striped" colorScheme="white" >
-            <Thead>
-              <Tr>
-                <Th>Lenguaje de programacion</Th>
-                <Th>No usamos esta herramienta</Th>
-                <Th>Usamos esta herramienta y nos gusta</Th>
-                <Th>Usamos esta herramienta y <u>NO</u> nos gusta</Th>
-                <Th>Nos gustaria tener esta herramienta en el futuro</Th>
-             </Tr>
-            </Thead>
-            <Tbody >
-              <Tr>
-                <Td>EJEMPLO LENGUAJE</Td>              
-                <Td ><Checkbox defaultIsChecked></Checkbox></Td>
-                <Td><Checkbox ></Checkbox></Td>
-                <Td><Checkbox ></Checkbox></Td>
-                <Td><Checkbox ></Checkbox></Td>
-             </Tr>
-             <Tr>
-                <Td>EJEMPLO LENGUAJE</Td>              
-                <Td ><Checkbox defaultIsChecked></Checkbox></Td>
-                <Td><Checkbox ></Checkbox></Td>
-                <Td><Checkbox ></Checkbox></Td>
-                <Td><Checkbox ></Checkbox></Td>
-             </Tr>
-             <Tr>
-                <Td>EJEMPLO LENGUAJE</Td>              
-                <Td ><Checkbox defaultIsChecked></Checkbox></Td>
-                <Td><Checkbox ></Checkbox></Td>
-                <Td><Checkbox ></Checkbox></Td>
-                <Td><Checkbox ></Checkbox></Td>
-             </Tr>
-            </Tbody>
-          </Table>
-
-          <Box paddingTop = "10px">
-            <Flex>
-              <Button colorScheme="gray_dark" backgroundColor="gray_dark" flex="1">Seccion Previa</Button>
-              <Spacer /> <Spacer />
-              <Button colorScheme="gray_dark" backgroundColor="gray_dark" flex="1">Siguiente Seccion</Button>
-            </Flex>
-          </Box>
-
         </Flex>
-      </Flex>  
-      
-           
+      </Flex>       
     </Flex>
-    
-    
   )
 }
-
-
 
 <Text fontSize="x1" color="white" marginBottom = "15px">Al crear tu cuenta podras navegar libremente{" "}
   <Link color="cornflowerblue" href="#">   Beneficios </Link>
 </Text>
 
-export default Signin
+export default formulario_01
