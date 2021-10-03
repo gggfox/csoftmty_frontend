0e7651cd2a16465dcabd2e647c2c381f01b7c84b
Antoniogm0898 <31547248+Antoniogm0898@users.noreply.github.com>
2021-10-02 19:58:37 -0500

    Cambios de color
    Cambios de color en el diseño

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Heading, Input, Link, Text, Image } from '@chakra-ui/react'
+import { Button, Flex, Heading, Input, Link, Text, Img } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import React from 'react'
 import logo from '../public/img/logo.png'
@@ -10,15 +10,15 @@ const Login: NextPage = () => {
     <Flex bg="white_dark" h='100vh' flexDirection="column">
       <NavBar></NavBar>
       <Flex height="100vh" alignItems="center" justifyContent="center">
-        <Flex direction="column" bg="linen" p={12} rounded={6}>
-          <Image src="public\img\logo.png" alt="logo" width="160" height="50" />
-          <Heading ma={15} mb={6} color="Spanish Gray" as="b" marginBottom = "1px">Bienvenido de vuelta!</Heading>
-          <Text fontSize="x1" color="Spanish Gray" marginBottom = "15px">Ingrese a su cuenta para navegar libremente</Text>
-          <Text fontWeight="semibold" color="Spanish Gray">Email</Text>
-          <Input placeholder="mail@ejemplo.com" _placeholder={{"color":"Spanish Gray"}} variant="filled" mb={3} type="email" bg="grey_light" 
+        <Flex direction="column" bg="#7B7B7B" p={12} rounded={6}>
+          <Img src="..\public\img\logo.png"  width="160" height="50" />
+          <Heading ma={15} mb={6} color="white" as="b" marginBottom = "1px">Bienvenido de vuelta!</Heading>
+          <Text fontSize="x1" color="white" marginBottom = "15px">Ingrese a su cuenta para navegar libremente</Text>
+          <Text fontWeight="semibold" color="white">Email</Text>
+          <Input placeholder="mail@ejemplo.com" _placeholder={{"color":"white"}} variant="filled" mb={3} type="email" bg="#7B7B7B" 
           borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
-          <Text fontWeight="semibold" color="Spanish Gray">Contraseña</Text>
-          <Input placeholder="********" _placeholder={{"color":"Spanish Gray"}} variant="filled" mb={6} type="password" bg="grey_light" 
+          <Text fontWeight="semibold" color="white">Contraseña</Text>
+          <Input placeholder="********" _placeholder={{"color":"white"}} variant="filled" mb={6} type="password" bg="#7B7B7B" 
           borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
           <Button colorScheme="orange" backgroundColor="orange" mb={6}>Ingresar a cuenta</Button>
           <Link color="white" align="center">Todavia no tengo cuenta</Link>
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,5 +1,5 @@
-import { Button, Flex, Heading, Input, Link, Text, Image, Stack, SimpleGrid, Box, NumberInput, NumberInputStepper } from '@chakra-ui/react'
-import { NumberInputField, NumberIncrementStepper, NumberDecrementStepper, Select } from '@chakra-ui/react'
+import { Button, Flex, Heading, Input, Link, Text, Stack, SimpleGrid} from '@chakra-ui/react'
+import { IconButton, Select } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import React from 'react'
 import { NavBar } from '../components/NavBar'
@@ -10,48 +10,43 @@ const Signin: NextPage = () => {
     <Flex bg="white_dark" h='100vh' flexDirection="column">
       <NavBar></NavBar>
       <Flex height="200vh" width = "200vh" alignItems="center" justifyContent="center">
-        <Flex direction="column" bg="linen" p={12} rounded={6}>
-          <Heading ma={15} mb={6} color="Spanish Gray" as="b" marginBottom = "1px">Registrate ahora</Heading>
-          <Text fontSize="x1" color="Spanish Gray" marginBottom = "15px">Al crear tu cuenta podras navegar libremente.  {" "}
+        <Flex direction="column" bg="#7B7B7B" p={12} rounded={6}>
+          <Heading ma={15} mb={6} color="white" as="b" marginBottom = "1px">Registrate ahora</Heading>
+          <Text fontSize="x1" color="white" marginBottom = "15px">Al crear tu cuenta podras navegar libremente.  {" "}
             <Link size ="sm" color="cornflowerblue" href="#">   Beneficios </Link>
           </Text>
 
-          <Text fontWeight="bold" fontSize="lg" color="Spanish Gray">Datos de la empresa</Text>
+          <Text fontWeight="bold" fontSize="lg" color="white">Datos de la empresa</Text>
           <Stack spacing={3} marginBottom = "20px">
-              <Text fontWeight="semibold" fontSize="sm" color="Spanish Gray">Nombre de la empresa</Text>
-              <Input placeholder="Nombre" _placeholder={{"color":"Spanish Gray"}} variant="outline" mb={3} type="text" bg="grey_light"
+            <SimpleGrid columns={2} spacingX="20px" spacingY="10px">
+              <Text fontWeight="semibold" fontSize="sm" color="white">Nombre</Text>
+              <Text fontWeight="semibold" fontSize="sm" color="white">Telefono</Text>
+              <Input placeholder="Escribe el nombre de la empresa" _placeholder={{"color":"white"}} variant="outline" mb={3} type="text" bg="#7B7B7B"
+                borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
+              <Input placeholder="8112345678" _placeholder={{"color":"white"}} variant="outline" mb={3} type="text" bg="#7B7B7B"
+                borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
+              <Text fontWeight="semibold" fontSize="sm" color="white">Email</Text>
+              <Text fontWeight="semibold" fontSize="sm" color="white">Sector de industria</Text>
+              <Input placeholder="ejemplo@gmail.com" _placeholder={{"color":"white"}} variant="outline" mb={3} type="email" bg="#7B7B7B"
+                borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
+              <Select placeholder="Elige una..." _placeholder={{"color":"white"}} bg="#7B7B7B" border="4px" borderColor="white" textColor="black" focusBorderColor="white" >
+                <option value="option1">Computer Hardware</option>
+                <option value="option1">Hosting and email"</option>
+                <option value="option1">IT Managment</option>
+                <option value="option1">Software Engineering"</option>
+              </Select>
+            </SimpleGrid>
+            <Text fontWeight="semibold" fontSize="sm" color="white">Sitio Web</Text>
+            <Input placeholder="https://ejemplo.com" _placeholder={{"color":"white"}} variant="outline" mb={3} type="email" bg="#7B7B7B"
               borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
-
-              <Text fontWeight="semibold" fontSize="sm" color="Spanish Gray">RFC</Text>
-              <Input placeholder="RFC" _placeholder={{"color":"Spanish Gray"}} variant="outline" mb={3} type="text" bg="grey_light"
-              borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
-
-              <Text fontWeight="semibold" fontSize="sm" color="Spanish Gray">Correo de la empresa</Text>
-              <Input placeholder="Correo" _placeholder={{"color":"Spanish Gray"}} variant="outline" mb={3} type="email" bg="grey_light"
-              borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
-
-              <Text fontWeight="semibold" fontSize="sm" color="Spanish Gray">Numero telefonico</Text>
-              <Input placeholder="Telefono #1" _placeholder={{"color":"Spanish Gray"}} variant="outline" mb={3} type="tel" bg="grey_light"
-              borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
-
-              <Text fontWeight="semibold" fontSize="sm" color="Spanish Gray">Numero telefonico alternativo</Text>
-              <Input placeholder="Telefono #2" _placeholder={{"color":"Spanish Gray"}} variant="outline" mb={3} type="tel" bg="grey_light"
-              borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
-
-          <SimpleGrid columns={2} spacingX="40px" spacingY="20px">
-            <Text fontWeight="semibold" fontSize="sm" color="Spanish Gray">Numero de empleados</Text>
-            <Text fontWeight="semibold" fontSize="sm" color="Spanish Gray">Clasificacion de la empresa</Text>
-            <NumberInput size="sm" maxW={16} defaultValue={1} min={1} max={1000}>
-              <NumberInputField bg="grey_light"/><NumberInputStepper><NumberIncrementStepper /><NumberDecrementStepper /></NumberInputStepper>
-            </NumberInput>
-            <Select placeholder="Elige una..." size="sm" bg="grey_light">
-              <option value="option1">Computer Hardware</option>
-              <option value="option1">Hosting and email"</option>
-              <option value="option1">IT Managment</option>
-              <option value="option1">Software Engineering"</option>
-            </Select>
-          </SimpleGrid>
-
+            <SimpleGrid columns={2} spacingX="20px" spacingY="10px">
+              <Text fontWeight="semibold" color="white">Contraseña</Text>
+              <Text fontWeight="semibold" color="white">Confirmacion</Text>
+              <Input placeholder="mas de 8 caracteres" _placeholder={{"color":"white"}} variant="filled" mb={6} type="password" bg="#7B7B7B" 
+               borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
+              <Input placeholder="igual que contraseña" _placeholder={{"color":"white"}} variant="filled" mb={6} type="password" bg="#7B7B7B" 
+               borderColor="white" border="4px" focusBorderColor="white" _hover={{"bg":"ghostwhite"}} textColor="black"/>
+            </SimpleGrid>
           </Stack>
 
 
@@ -62,7 +57,7 @@ const Signin: NextPage = () => {
   )
 }
 
-<Text fontSize="x1" color="Spanish Gray" marginBottom = "15px">Al crear tu cuenta podras navegar libremente{" "}
+<Text fontSize="x1" color="white" marginBottom = "15px">Al crear tu cuenta podras navegar libremente{" "}
   <Link color="cornflowerblue" href="#">   Beneficios </Link>
 </Text>
 
