b6e92e0c276462946e96b7972be0f47de3dfe1a7
Gerardo Galan Garzafox <gerardogalangarzafox@gmail.com>
2021-10-14 17:49:39 -0500

    Changed routes for categoria and minor navbar bugfix relating to z-index

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,8 +13,10 @@ interface NavBarProps {
 export const NavBar : React.FC<NavBarProps> = ({ }) => {
     const router = useRouter()
     const path = router.pathname
+
+    const user_id = 1 // luego necesitamos un hook o algo para obtener el user id y guardarlo en el cliente
     return (
-        <Flex bg="white_light"  justifyContent="space-between" alignItems="center" boxShadow="md" position="fixed" w="100vw" flexDirection="row" h={16}>
+        <Flex bg="white_light"  justifyContent="space-between" alignItems="center" boxShadow="md" position="fixed" w="100vw" zIndex={1} flexDirection="row" h={16}>
             
             <NextLink href="/"><Link mx={5} my={2}>
             <Image src={logo} alt="logo" width="160" height="50"/></Link>
@@ -39,29 +41,29 @@ export const NavBar : React.FC<NavBarProps> = ({ }) => {
 
 
             <Menu autoSelect={false}>
-            <MenuButton color="black_dark"  fontFamily="lato" borderColor="orange" borderBottomWidth={ path == '/datosNL' ? 5 : 0}  mr={5} p="6px"
+            <MenuButton color="black_dark"  fontFamily="lato" borderColor="orange" borderBottomWidth={ path.slice(0,11) == '/categorias' ? 5 : 0}  mr={5} p="6px"
             _expanded={{"bg":"black", "color":"white", "borderRadius":"6px"}}
             >Categorias</MenuButton>
             <Portal>
                 <MenuList bg="black" color="white_light" borderWidth={0}>
-                <NextLink href="/desarrollodesoftware">
+                <NextLink href="/categorias/desarrollodesoftware">
                     <MenuItem _hover={{"bg":"orange"}}>Desarrollo de Software</MenuItem>
                 </NextLink>
-                <NextLink href="/hardware">
+                <NextLink href="/categorias/hardware">
                     <MenuItem _hover={{"bg":"orange"}}>Hardware</MenuItem>
                 </NextLink>
-                <NextLink href="/servicios">
+                <NextLink href="/categorias/servicios">
                     <MenuItem _hover={{"bg":"orange"}}>Servicios</MenuItem>
                 </NextLink>
-                <NextLink href="/tecnologia4">
+                <NextLink href="/categorias/tecnologia4">
                     <MenuItem _hover={{"bg":"orange"}}>Tecnologia 4.0</MenuItem>
                 </NextLink>
                 </MenuList>
             </Portal>
             </Menu>
 
-              <NextLink href="/cuenta">
-                <Link color="black_dark" fontFamily="lato" borderColor="orange" borderBottomWidth={ path == '/cuenta' ? 5 : 0}  mr={6} >
+              <NextLink href="/cuenta/[id]" as={`/cuenta/${user_id}`}>
+                <Link color="black_dark" fontFamily="lato" borderColor="orange" borderBottomWidth={ path == '/cuenta/[id]' ? 5 : 0}  mr={6} >
                     <Flex h="100%" alignItems="center" justifyContent="center">
                         <Text>
                             Mi cuenta
